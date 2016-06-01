4fb8ce644cd129d8d2b92eac73e79dc4fcb8ab58
krandri <k.randriani@gmail.com>
2016-06-02 00:12:04 +0200

    modif sur test

--- TestArduino/app/src/main/java/com/example/kevin/testarduino/MainActivity.java
+++ TestArduino/app/src/main/java/com/example/kevin/testarduino/MainActivity.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 
 public class MainActivity extends Activity {
 //    private final String DEVICE_NAME="MyBTBee";
-    private final String DEVICE_ADDRESS="20:13:10:15:33:66";
+    private final String DEVICE_ADDRESS="14:2:260191";
     private final UUID PORT_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");//Serial Port Service ID
     private BluetoothDevice device;
     private BluetoothSocket socket;
